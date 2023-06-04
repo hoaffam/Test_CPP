f5f90173ec3f792fb52263f9e5e26b8b0adc60c4
hoaffam <2000thaihoa@gmail.com>
2023-06-04 14:03:24 +0700

    Update Flight_Connections

--- Flight_Connections/source.cpp
+++ Flight_Connections/source.cpp
@@ -3,6 +3,9 @@
 //#define RunTests 1
 using namespace std;
 //Function to check the connection from vertex i to vertex j in the matrix
+//When using references (&), a function parameter can be passed to a function by passing a reference to the original object 
+//without creating a new copy. 
+//This can help save time and resources by avoiding unnecessary copying of the object.
 int checkConnections(const vector<vector<bool>>& matrix, int i, int j)
 {
 
@@ -11,7 +14,7 @@ int checkConnections(const vector<vector<bool>>& matrix, int i, int j)
         return true;
     }
     else{
-        for(int k = 0; k < matrix.size(); k++) //ne
+        for(int k = 0; k < matrix.size(); k++) 
         {
             if(matrix[i][k])
             {
@@ -31,16 +34,16 @@ int getMinimumConnections(const vector<vector<bool>>& matrix)
     // throw std::logic_error("Waiting to be implemented");
     int count = 0; //number of new created connections if exist 0 connection
     vector<vector<bool>> copy = matrix;
-    int startingPoint = 0;
+    int start_Point = 0;
     for(int i = 0; i < matrix.size(); i++)
     {
-        if(checkConnections, startingPoint, i)
+        if(checkConnections, start_Point, i)
         {
             continue;
         }
         else{
-            copy[startingPoint][i] = 1;
-            copy[i][startingPoint] = 1;
+            copy[start_Point][i] = 1;
+            copy[i][start_Point] = 1;
             count++;
         }
     }
@@ -57,6 +60,6 @@ int main()
         {false, false, true, false, false},
         {true, false, false, false, false}
     };
-    cout << getMinimumConnections(matrix) << endl; // should print 1
+    cout << getMinimumConnections(matrix) << endl; //1
 }
 // #endif
